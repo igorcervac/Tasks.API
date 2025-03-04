90b76c896f7dd0e6edab34cda9df372b1e373433
igorcervac <igor.cervac@outlook.com>
2025-03-04 13:22:54 -0500

    Implement Tasks controller

---
+++ Tasks.API/Models/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Tasks.API.Models
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
--- Tasks.API/Program.cs
+++ Tasks.API/Program.cs
@@ -1,4 +1,6 @@
 
+using Tasks.API.Models;
+
 namespace Tasks.API
 {
     public class Program
@@ -13,6 +15,8 @@ namespace Tasks.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IGenericRepository<Models.Task>, InMemoryGenericRepository<Models.Task>>();
 
             var app = builder.Build();
 
