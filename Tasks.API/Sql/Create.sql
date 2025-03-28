a7688fae7455892f5d66849be827a7edca6b46d6
igorcervac <igor.cervac@outlook.com>
2025-03-28 19:24:54 -0400

    Implement sql script

---
+++ Tasks.API/Sql/Create.sql
@@ -0,0 +1,18 @@
+﻿USE [Subscription1DB]
+GO
+
+CREATE SCHEMA Tasks
+GO
+
+CREATE TABLE [Tasks].[States](Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, [Name] NVARCHAR(256))
+GO
+
+INSERT INTO [Tasks].[States]([Name]) VALUES ('To Do'), ('In Progress'), ('Completed')
+
+CREATE TABLE [Tasks].[Tasks](
+Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+[Title] NVARCHAR(1024) NULL, 
+[Description] NVARCHAR(MAX) NULL, 
+[StateId] INT NOT NULL, 
+FOREIGN KEY([StateId]) REFERENCES  [Tasks].[States](Id))
+GO
